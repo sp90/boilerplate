bd2e1007af3a09122e3b90836322abaa69a7aeb1
simio90 <simonpetersen3@hotmail.co.uk>
2014-06-24 12:55:04 +0200

    startup project boilerplate files

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            main:{
+                src: [
+                  'public/js/libs/angular.js',
+                  'public/js/libs/angular-route.js',
+                  'public/js/libs/modernizr-v2.8.3.js',
+                  'public/js/libs/jquery-v1.11.1.js',
+                  'public/js/libs/lodash.compat-v2.4.1.js',
+                  'public/js/main.js'
+                ],
+                dest: 'public/js/build/all.js'
+            }
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                mangle: {
+                    except: ['jQuery', 'angular']
+                }
+            },
+            dist: {
+                files: {
+                    'public/js/build/all.min.js': ['<%= concat.main.dest %>']
+                }
+            }
+        },
+        clean: ['.sass-cache']
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('build:prod', ['concat', 'uglify', 'clean']);
+};
+
---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var connect = require('connect');
+var http = require('http');
+var bodyParser = require('body-parser');
+var gzip = require('connect-gzip');
+var methodOverride = require('method-override');
+var app = express();
+
+var hbs = require('express-hbs');
+var Datastore = require('nedb'), 
+	db = new Datastore({ filename: 'db/mydb.db', autoload: true });
+
+app.engine('hbs', hbs.express3({
+  	partialsDir: __dirname + '/views/partials',
+    defaultLayout: __dirname + '/views/layout.hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(process.cwd() + '/public'));
+gzip.staticGzip(__dirname + '/public', { maxAge: 86400000 }); //86400000ms = 1 day
+//app.use(bodyParser.json());
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// override with the X-HTTP-Method-Override header in the request
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride());
+
+
+app.get('/', function(req, res){
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	var returnObj = {
+		metaSetup: {
+			author: "www.nomis.dk - sp90(github)",
+			title: "Boilerplate",
+			pagetitle: "index",
+			description: "This is a basic boilerplate which imply the best practice in frontend and a nodeJS express server",
+			pageurl: fullUrl,
+			imgurl: "imgurl"
+		}
+	}
+  	res.render("index", returnObj);
+});
+
+app.listen(3000, function(err){
+	if(err) {
+		console.log(err);
+	} else {
+		console.log("server started on localhost:3000");
+	}	
+});
